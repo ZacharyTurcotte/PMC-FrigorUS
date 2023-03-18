45c1dbb919dfaa65a103af1206db47b7bdc554c4
ZacharyTurcotte <zachary.turcotte@usherbrooke.ca>
2023-03-18 14:54:03 -0400

    update rtc

--- Raspberry_Pi/PCA850073ADP.py
+++ Raspberry_Pi/PCA850073ADP.py
@@ -1,5 +1,4 @@
-import smbus
-import time
+import smbus2
 
 #Registre
 #Control_1 00h
@@ -14,22 +13,23 @@ import time
 #Months 09h
 #Years 0Ah
 
-#ressource https://www.nxp.com/products/peripherals-and-logic/signal-chain/real-time-clocks/automotive-rtcs/automotive-tiny-real-time-clock-calendar-with-alarm-function-and-ic-bus:PCA85073A
-
+#ressource : https://www.nxp.com/products/peripherals-and-logic/signal-chain/real-time-clocks/automotive-rtcs/automotive-tiny-real-time-clock-calendar-with-alarm-function-and-ic-bus:PCA85073A
+#datasheet : https://www.nxp.com/docs/en/data-sheet/PCA85073A.pdf
 
 
 class PCA85073ADP:
     def __init__(self, bus_num=1, rtc_READ_address=0xA3,rtc_WRITE_address=0xA2): 
         self.rtc_write_address = rtc_WRITE_address
         self.rtc_read_address = rtc_READ_address
-        self.bus = smbus.SMBus(bus_num)
+        self.bus = smbus2.SMBus(bus_num)
         control_1 = 0b10000000 # a changer
         control_2 = 0b00000000 #bit 0 a 0 aucun interupts d'alarme
         self.bus.write_i2c_block_data(self.rtc_address,0x00,control_1)
         self.bus.write_i2c_block_data(self.rtc_address,0x00,control_2)
     
     def read_datetime(self):
-        rtc_data = self.bus.write_i2c_block_data(self.rtc_write_address, 0x04)
+        
+        self.write_byte(self.rtc_write_address,0x04)
 
         seconds = (rtc_data[0] & 0x7f) + (rtc_data[6] & 0x80)
         minutes = rtc_data[1] & 0x7f
@@ -42,6 +42,7 @@ class PCA85073ADP:
         return (year, month, date, day, hours, minutes, seconds)
 
     def write_datetime(self, year, month, date, day, hours, minutes, seconds):
+        
         rtc_data = [0] * 7
         rtc_data[0] = (seconds % 60) | 0x80
         rtc_data[1] = minutes % 60
