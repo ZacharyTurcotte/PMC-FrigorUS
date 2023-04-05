e67b24bc0361036d08c6956892533ee7c690e9a5
ZacharyTurcotte <zachary.turcotte@usherbrooke.ca>
2023-04-05 15:40:27 -0400

    update can bus

--- Raspberry_Pi/CAN_RPI.py
+++ Raspberry_Pi/CAN_RPI.py
@@ -11,18 +11,20 @@ class CAN_driver:
 
 		if int(bitrate) != 125000 and int(bitrate) != 250000 and int(bitrate) != 500000 and int(bitrate) != 1000000: 
 			raise ValueError("Bitrate invalide")
-		
+		 
 		self.bitrate = str(bitrate)
 		self.CAN_channel = str(canX)
 		
 		#os.system('sudo ip link set ' + str(canX) + ' type can bitrate ' +str(bitrate))
 		#os.system('sudo ifconfig '+ str(canX) + ' up')
 		self.can0 = can.interface.Bus(channel = self.CAN_channel ,bustype='socketcan')
-		
+
+
 	def send_can_data_frame(self,id,data):
 		msg1 = can.Message(arbitration_id=id, data=data,is_extended_id=False)	
 		self.can0.send(msg1)
 		time.sleep(0.01) #pour laisser le temps d'envoyer le frame
+
 
 	def send_remote_frame(self,id):
 		msg1 = can.Message(arbitration_id=id,is_extended_id=False,is_remote_frame=True)	
@@ -42,7 +44,7 @@ class CAN_driver:
 			out[i] =  msg.data[i]
 		return out
 
+
 	def  __read_can_bus(self,timeout):
 		msg = self.can0.recv(timeout)
 		return msg
-
